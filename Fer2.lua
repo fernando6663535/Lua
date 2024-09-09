452839308276057e386564882cd39cd5f499fc5f
fernando6663535 <151690401+fernando6663535@users.noreply.github.com>
2024-09-08 22:16:23 -0500

    Delete Fer2.lua

--- Fer2.lua
+++
@@ -1,5 +0,0 @@
-local PlaceId = game.PlaceId  
-if PlaceId == 5151400895 then
-    wait(6.5)
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Fernanflop091o/PARA_AMIGOS-ASER-SCRITP/main/Fern.lua"))()
-end
